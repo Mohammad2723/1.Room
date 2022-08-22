9072d097e8587f183c115eb816bb75dc54ba5b06
ebrahimi16153 <ebrahimi2723@yahoo.com>
2022-08-22 22:39:21 +0430

    listView MainActivity.kt implement

--- app/src/main/java/ebrahimi16153/github/com/room/MainActivity.kt
+++ app/src/main/java/ebrahimi16153/github/com/room/MainActivity.kt
@@ -3,11 +3,27 @@ package ebrahimi16153.github.com.room
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import ebrahimi16153.github.com.room.adapter.UserAdapter
 import ebrahimi16153.github.com.room.databinding.ActivityAddUserBinding
 import ebrahimi16153.github.com.room.databinding.ActivityMainBinding
+import ebrahimi16153.github.com.room.db.UserDatabase
+import ebrahimi16153.github.com.room.util.USER_DATABASE
 
 class MainActivity : AppCompatActivity() {
+
+    private val db :UserDatabase by lazy { Room.databaseBuilder(this,UserDatabase::class.java,
+        USER_DATABASE)
+        .fallbackToDestructiveMigration()
+        .allowMainThreadQueries()
+        .build()
+
+    }
     lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,13 +31,36 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.apply {
-
             fabAdd.setOnClickListener {
                 startActivity(Intent(this@MainActivity, AddUserActivity::class.java))
             }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        isListEmpty()
+    }
+
+
+    private fun isListEmpty() {
+        if (db.dao().getAllUser().isNotEmpty()) {
+            binding.apply {
+                userList.visibility = View.VISIBLE
+                emptyTxt.visibility = View.GONE
+                val  adapter = UserAdapter()
+                adapter.diff.submitList(db.dao().getAllUser())
+                userList.adapter = adapter
+                userList.layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+        } else {
+            binding.apply {
+                userList.visibility = View.VISIBLE
+                emptyTxt.visibility = View.GONE
+            }
 
         }
-
-
     }
+
 }
--- app/src/main/java/ebrahimi16153/github/com/room/adapter/UserAdapter.kt
+++ app/src/main/java/ebrahimi16153/github/com/room/adapter/UserAdapter.kt
@@ -1,5 +1,52 @@
 package ebrahimi16153.github.com.room.adapter
 
-class UserAdapter {
-    //TODO
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import ebrahimi16153.github.com.room.databinding.UserListItemBinding
+import ebrahimi16153.github.com.room.db.UserEntity
+
+class UserAdapter: RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            UserListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setViews(diff.currentList[position])
+    }
+
+    override fun getItemCount() =diff.currentList.size
+
+    inner class ViewHolder(private val binding: UserListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun setViews(item: UserEntity) {
+            binding.nameItem.text = item.userName
+            binding.ageItem.text = item.userAge.toString()
+        }
+
+
+    }
+
+    private val diffCalBack = object : DiffUtil.ItemCallback<UserEntity>() {
+        override fun areItemsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val diff = AsyncListDiffer(this,diffCalBack)
+
+
 }
